a7f03a71f92f0f48d702171b51484ecbeea9efad
wlgoncharenko <wladimir.ua@gmail.com>
2017-05-15 23:39:35 +0300

    HomeWork01

---
+++ HW01/src/script.js
@@ -0,0 +1,18 @@
+var Vladimir = {
+    firstName: 'Vladimir',
+    lastName: 'Goncharenko',
+    businesGroup: true,
+    age: 40,
+    phoneNumbers: [+380975569646, +380502856737],
+    adress: {
+        zip: 61058,
+        city: 'Kharkiv',
+        street: 'Very Best',
+        home: 77
+    },
+    eMail: 'wladimir.ua@gmail.com'
+}
+console.log(Vladimir);
+console.log(Vladimir.lastName);
+console.log(Vladimir.phoneNumbers[1]);
+console.log(Vladimir.adress.street);
